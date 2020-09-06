334decbee09cd9d8da980edf58ac0537f21cb6b9
KangChangYi <1004845773@qq.com>
2020-09-06 15:43:49 +0800

    feat(title): modify blog title

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: '特辣的博客',
+    title: 'KangChangYi',
     description: 'Step by step',
     // base: "/Knowledge-Base/",
     base: "/",
@@ -10,40 +10,5 @@ module.exports = {
         ],
         sidebarDepth: 0,
         sidebar: require('../siderbarList.json')
-        // sidebar: [
-        //     {
-        //         title: 'JavaScript',
-        //         children: [
-        //             '/JavaScript/ES6上.md',
-        //         ]
-        //     },
-        //     {
-        //         title: 'CSS',
-        //         children: [
-        //             '/CSS/盒子模型.md',
-        //             '/CSS/块级元素与行内元素.md',
-        //             '/CSS/BFC.md',
-        //             '/CSS/选择器.md'
-        //         ]
-        //     },
-        //     {
-        //         title: '网络',
-        //         children: [
-        //             '/网络/三次握手四次挥手.md',
-        //         ]
-        //     }, 
-        //     {
-        //         title: '浏览器',
-        //         children: [
-        //             '/浏览器/浏览器缓存.md',
-        //         ]
-        //     },
-        //     {
-        //         title: '数据结构',
-        //         children: [
-        //             '/数据结构/栈.md',
-        //         ]
-        //     },
-        // ]
     },
 }
