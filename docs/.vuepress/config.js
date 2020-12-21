58c6a82950aaa10f6b52f362dfc422a88d2fc206
kangchangyi <1004845773@qq.com>
2020-12-21 09:49:49 +0800

    chore(config): 修改导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,19 @@
 module.exports = {
-    title: 'KangChangYi',
-    description: 'Step by step',
-    // base: "/Knowledge-Base/",
-    base: "/",
-    themeConfig: {
-        nav: [
-            { text: 'GitHub', link: 'https://github.com/KangChangYi' },
-            { text: 'Blog', link: 'https://kangchangyi.github.io/' },
-        ],
-        sidebarDepth: 0,
-        sidebar: require('../siderbarList.json')
-    },
-}
+  title: "KangChangYi",
+  description: "Step by step",
+  // base: "/Knowledge-Base/",
+  base: "/",
+  themeConfig: {
+    nav: [
+      { text: "Guide", link: "./guide.md" },
+      {
+        text: "GitHub",
+        link: "https://github.com/KangChangYi",
+        target: "_blank",
+      },
+    ],
+    sidebarDepth: 0,
+    sidebar: require("../siderbarList.json"),
+  },
+};
+ 
